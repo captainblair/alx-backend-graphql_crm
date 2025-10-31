baf9764357b9a95dfe53d3aa8bff1dade24c56c3
Engineer Tony <wangolotony4@gmail.com>
2025-10-31 20:18:17 +0300

    Fix Task 2: Add GraphQL hello field query to heartbeat logger

--- crm/cron.py
+++ crm/cron.py
@@ -1,8 +1,19 @@
 from datetime import datetime
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 def log_crm_heartbeat():
     """Log heartbeat message to confirm CRM application health"""
     timestamp = datetime.now().strftime('%d/%m/%Y-%H:%M:%S')
+    
+    try:
+        transport = RequestsHTTPTransport(url="http://localhost:8000/graphql")
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        query = gql("{ hello }")
+        client.execute(query)
+    except:
+        pass
+    
     message = f"{timestamp} CRM is alive\n"
     
     with open('/tmp/crm_heartbeat_log.txt', 'a') as log_file:
